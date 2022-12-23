9cf2d121e9b1e90895d19ee6d9156c369be6d4c2
Arnol Plazas <plazasarnol@gmail.com>
2022-12-22 22:14:45 -0500

    I used FastAPI to create my first backend.

--- webserver/main.py
+++ webserver/main.py
@@ -1,5 +1,22 @@
 import store
 
+from fastapi import FastAPI 
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1, 2, 3, 4]
+
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list_contact():
+    return """
+        <h1>Hi I am a page web</h1>
+        <p>I am a paragraph.</p>
+    """
+
 def run():
     store.get_categories()
 
