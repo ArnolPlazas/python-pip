804cd8127f08829bec8dd25d78fe4cc8960d0eb0
Arnol Plazas <plazasarnol@gmail.com>
2022-12-22 21:51:24 -0500

    I used Pandas library to read csv file.

--- read_files/charts.py
+++ read_files/charts.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 def generate_pie_chart(labels, values):
     fig, ax = plt.subplots()
     ax.pie(values, labels = labels)
-    plt.savefig(f'./read_files/imgs/pie.png')
+    plt.savefig(f'./imgs/pie.png')
     plt.close()
 
 
 def generate_bar_chart(name, labels, values):
     fig, ax = plt.subplots()
     ax.bar(labels, values)
-    plt.savefig(f'./read_files/imgs/{name}.png')
+    plt.savefig(f'./imgs/{name}.png')
     plt.close()
 
 if __name__ == '__main__':
--- read_files/main.py
+++ read_files/main.py
@@ -2,17 +2,27 @@ import read_csv
 import charts
 import utils
 
+import pandas as pd
+
 
 def run():
+    """ 
     data = read_csv.read_csv('./read_files/data.csv')
     data = list(filter(lambda item: item['Continent'] == 'South America', data))
 
     countries = list(map(lambda x: x['Country'], data))
     percentages = list(map(lambda x: x['World Population Percentage'], data))
+    """
+    
+    df = pd.read_csv('./data.csv')
+    df = df[df['Continent'] == 'Africa']
+
+    countries = df['Country'].values
+    percentages = df['World Population Percentage'].values
 
     charts.generate_pie_chart(countries, percentages)
 
-
+    """
     country = input('Type Country => ')
 
     result = utils.population_by_country(data, country)
@@ -20,8 +30,9 @@ def run():
     if len(result) > 0:
         country = result[0]
         labels, values = utils.get_population(country)
-        charts.generate_bar_chart(country['Country'], labels, values)
-
+        charts.generate_bar_chart(country['Country'], labels, values) 
+    """
+    
 
 if __name__ == '__main__':
     run()
